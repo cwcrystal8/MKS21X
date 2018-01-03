f84cc877a2591f603117c94051c0629d7f1ae807
Crystal Wang <cwang4@stuy.edu>
2018-01-02 21:08:33 -0500

    Fixed variable error in bogoSort(int[] data)

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -40,11 +40,11 @@ public class Sorts{
 
     public static void bogoSort(int[] data){
 	while(!(isSorted(data))){
-	    for(int i = 0 ; i < ary.length; i++){
-		int temp = ary[i];
-		int newSpot = (int)(Math.random()*ary.length);
-		ary[i] = ary[newSpot];
-		ary[newSpot] = temp;
+	    for(int i = 0 ; i < data.length; i++){
+		int temp = data[i];
+		int newSpot = (int)(Math.random()*data.length);
+		data[i] = data[newSpot];
+		data[newSpot] = temp;
 	    }
 	}
     }
